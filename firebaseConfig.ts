d11c3811bd0c67750a40b3c3698489689e06fdcf
SUN <st2000423@gmail.com>
2025-12-23 20:53:20 +0900

    Update task list and sync latest UI changes to GitHub

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "***REMOVED_API_KEY_6***",
@@ -15,3 +16,5 @@ const app = initializeApp(firebaseConfig);
 export const db = initializeFirestore(app, {
     localCache: persistentLocalCache()
 });
+
+export const auth = getAuth(app);
